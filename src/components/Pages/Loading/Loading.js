bacf93c147a2a3e53445c871cc1311b5a4eaf2c6
Tarequl Islam <tareque.dev@gmail.com>
2022-04-30 17:17:51 +0600

    LOADING COMPONENTS ADD

--- src/components/Pages/Loading/Loading.js
+++ src/components/Pages/Loading/Loading.js
@@ -2,7 +2,9 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div>
+    <div  style={{ height: "500px" }}
+    className="w-full flex aligns-center justify-center"
+  >
       <div class="flex justify-center items-center space-x-2">
         <div
           class="spinner-grow inline-block w-8 h-8 bg-current rounded-full opacity-0 text-blue-300"
